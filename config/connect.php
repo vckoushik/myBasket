6aaec14cffddb0e807c22d344378054dd31bfa3a
Koushik Siva <43028459+vckoushik@users.noreply.github.com>
2022-11-12 20:02:04 +0530

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('sql310.epizy.com', '	epiz_32983026', 'HzEgTsKRTb', 'epiz_32983026_ecomphp');
+$connection = mysqli_connect('sql310.epizy.com', 'epiz_32983026', 'HzEgTsKRTb', 'epiz_32983026_ecomphp');
 if(!$connection){
 	echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
